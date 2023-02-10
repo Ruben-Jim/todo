c931b8d8719eca0e69037450de22f0e3e24e0b53
FireCon <89361575+Ruben-Jim@users.noreply.github.com>
2023-02-10 13:34:50 -0800

    permanent

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,25 @@
+
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD2uJA7utegSL-SLjWAWeDFQuzZxbKKPtI",
-  authDomain: "todo-app-8f420.firebaseapp.com",
-  projectId: "todo-app-8f420",
-  storageBucket: "todo-app-8f420.appspot.com",
-  messagingSenderId: "390200850433",
-  appId: "1:390200850433:web:9c7c22d64e6bef8beca800"
+  apiKey: "AIzaSyCoRjcIVBIbRPetmqmwDXDTumQFio3Z0po",
+  authDomain: "todo-chat-c595d.firebaseapp.com",
+  projectId: "todo-chat-c595d",
+  storageBucket: "todo-chat-c595d.appspot.com",
+  messagingSenderId: "1095325965914",
+  appId: "1:1095325965914:web:d1edad0240812e4b708263",
+  measurementId: "G-8FWRM0GK3W"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app)
