e585f1c3fc9609f6bbacb2b31964fb296cdedd58
Ruben <ruben.jim.co@gmail.com>
2023-03-08 19:58:04 -0800

    setting up development

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +23,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app)
